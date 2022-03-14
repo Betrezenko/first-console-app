e20339e514970fbe5fa1c9c053040a548fc476ff
Betrezenko <betrezenko@gmail.com>
2022-03-14 15:32:25 +0300

    Add easy functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many films you've watched?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many films you've watched?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many films you've watched?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
         count: numberOfFilms,
@@ -10,27 +20,35 @@ const personalMovieDB = {
         privat: false
     };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Last wached film?", ""),
-        b = prompt("Rate it", "");
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Last wached film?", ""),
+            b = prompt("Rate it", "");
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Watched few films");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Casual user');
-} else if (personalMovieDB.count > 30) {
-    console.log('Fan');
-} else {
-    console.log('error');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Watched few films");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Casual user');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Fan');
+    } else {
+        console.log('error');
+    }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
