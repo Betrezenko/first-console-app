7f56f5da1b95d4aa3221e4865d737b7aa7f2b81a
Betrezenko <betrezenko@gmail.com>
2022-03-14 13:44:27 +0300

    Add check for film counter

--- js/script.js
+++ js/script.js
@@ -23,4 +23,14 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+if (personalMovieDB.count < 10) {
+    console.log("Watched few films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Casual user');
+} else if (personalMovieDB.count > 30) {
+    console.log('Fan');
+} else {
+    console.log('error');
+}
+
 console.log(personalMovieDB);
